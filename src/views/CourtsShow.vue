22ed3f7f2d706b243a9d1715af798f6630b01590
Josh Garcia <joshgarcia7410@gmail.com>
2021-08-06 14:11:41 -0500

    adds mapbox

--- src/views/CourtsShow.vue
+++ src/views/CourtsShow.vue
@@ -5,9 +5,16 @@
     <p>Fees: ${{ court.fees }}</p>
     <p>Facility: {{ court.facility }}</p>
     <p>Lights: {{ court.lights }}</p>
+    <div id="map"></div>
     <router-link to="/home">Back</router-link>
   </div>
 </template>
+<style>
+#map {
+  height: 300px;
+  width: auto;
+}
+</style>
 <script>
 import axios from "axios";
 
@@ -17,10 +24,34 @@ export default {
       court: {},
     };
   },
+  mounted: function () {
+    this.displayMap();
+  },
   created: function () {
     axios.get("/courts/" + this.$route.params.id).then((response) => {
       this.court = response.data;
     });
+  },
+  methods: {
+    displayMap: function () {
+      var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+      mapboxgl.accessToken = "pk.eyJ1Ijoid29ybW10IiwiYSI6ImNrczBrMzJycDFrbGoydm5wc2ZiamlqNGgifQ.I2AHKCY7cTV3-kuN2uUexQ";
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-97.753, 30.375],
+        zoom: 15,
+      });
+      console.log(map);
+      document.getElementById("map").addEventListener("click", () => {
+        // Fly to a random location by offsetting the point -74.50, 40
+        // by up to 5 degrees.
+        map.flyTo({
+          center: [-97.75, 30.37],
+          essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+        });
+      });
+    },
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,19 @@
     <h1>{{ message }}</h1>
     <p>Search by name</p>
     <input v-model="searchFilter" />
-    <div v-for="court in filterBy(courts, searchFilter, 'name')" :key="court.id">
-      <h1>{{ court.name }}</h1>
+    <div v-for="court in filterBy(courts, searchFilter, 'name')" :key="court.id" id="container">
+      <h1>
+        <hr />
+        <router-link v-bind:to="`/courts/${court.id}`">
+          {{ court.name }}
+        </router-link>
+      </h1>
       <p>{{ court.address }}</p>
       <dialog id="court-details">
         <h1>Court Details</h1>
         <p>Lights: {{ court.lights }}</p>
         <p>Fees: $ {{ court.fees }}</p>
         <p>Facility: {{ court.facility }}</p>
-        <p></p>
         <button>Close</button>
       </dialog>
       <button v-on:click="showCourt(court)">More Info</button>
@@ -32,6 +36,9 @@ export default {
       searchFilter: "",
     };
   },
+  // mounted: function () {
+  //   this.displayMap();
+  // },
   created: function () {
     this.indexCourts();
   },
@@ -48,6 +55,16 @@ export default {
       this.currentCourt = court;
       document.querySelector("#court-details").showModal();
     },
+    // displayMap: function () {
+    //   var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+    //   mapboxgl.accessToken = "pk.eyJ1Ijoid29ybW10IiwiYSI6ImNrczBrMzJycDFrbGoydm5wc2ZiamlqNGgifQ.I2AHKCY7cTV3-kuN2uUexQ";
+    //   var map = new mapboxgl.Map({
+    //     container: "map",
+    //     style: "mapbox://styles/mapbox/streets-v11",
+    //   });
+    //   console.log(map);
+    //},
   },
 };
 </script>
